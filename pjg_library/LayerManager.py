683365ee3d467494122b79857009ec5c9d6af6a2
Garrison Osteen <garosteen@gmail.com>
2026-07-03 22:53:00 -0600

    Add grid functionality

--- pjg_library/LayerManager.py
+++ pjg_library/LayerManager.py
@@ -25,8 +25,12 @@ What else could the LayerManager do for me?
 """
 class LayerManager:
 
-    def __init__(self, numcolors, width=5.0, height=7.0, margin=0.25, blend=False, cm=False):
-        self.n = numcolors
+    def __init__(self, numcolors, width=5.0, height=7.0, rows=1, cols=1, margin=0.25, blend=False, cm=False):
+        self.cells = []
+        self.rows = rows
+        self.cols = cols
+        self.current_cell = 0
+
         if (cm):
             self.width = width
             self.height = height
@@ -36,16 +40,60 @@ class LayerManager:
             self.width  = 2.54*width
             self.height = 2.54*height
             self.margin = 2.54*margin
+
+        # Create the cells:
+        self.col_width =  self.width / self.cols
+        self.row_height = self.height / self.rows
+        for row in range(rows):
+            for col in range(cols):
+                originx = col * self.col_width
+                originy = row * self.row_height
+                self.cells.append(LayerManagerCell((originx, originy), (self.col_width, self.row_height), self.margin))
+
+
+        self.centerx = self.col_width/2.0
+        self.centery = self.row_height/2.0
+
+    def add(self, geom, layer=None, cell=None):
+        if cell is None:
+            cell = self.current_cell
+
+        if cell > len(self.cells):
+            print("Cell number too high in LayerManager.add")
+            return
+        self.cells[cell].add(geom, layer)
+
+    def get(self, layer):
+        return GeometryCollection(self.layers[layer])
+
+    def draw_to_vsketch(self, vsk: vsketch.Vsketch):
+        for cell in self.cells:
+            cell.draw_to_vsketch(vsk)
+
+    def set_cell(self, cell):
+        self.current_cell = cell % len(self.cells)
+
+
+
+class LayerManagerCell:
+    def __init__(self, origin=(0,0), dimensions=(0,0), margin=0.25):
+        self.origin = origin
+        self.dimensions = dimensions
+        self.width = self.dimensions[0]
+        self.height = self.dimensions[1]
+        self.margin = margin
+        self.originx = self.origin[0]
+        self.originy = self.origin[1]
         self.centerx = self.width/2.0
         self.centery = self.height/2.0
 
+        self.layers = [[]]
+
+        # TODO: radius for edges?
         self.bound = Polygon([(self.margin, self.margin),
                               (self.width-self.margin,self.margin),
                               (self.width-self.margin,self.height-self.margin),
                               (self.margin,self.height-self.margin)])
-        self.layers = []
-        for i in range(numcolors):
-            self.layers.append([])
 
     """
     Add the specified geometry to the layer
@@ -53,10 +101,9 @@ class LayerManager:
     """
     def add(self, geom, layer=None):
         if layer is None:
-            layer = random.randint(0,self.n-1)
+            layer = random.randint(0,len(self.layers)-1)
         if geom.geom_type is "GeometryCollection":
             for element in geom.geoms:
-                #print(element.geom_type)
                 self.add_single_geom(element, layer)
         else:
             self.add_single_geom(geom, layer)
@@ -78,11 +125,12 @@ class LayerManager:
         self.layers[layer].append(geom)
 
     def draw_to_vsketch(self, vsk: vsketch.Vsketch):
+        vsk.pushMatrix()
+        vsk.translate(self.originx, self.originy)
         stroke = 1
         for layer in self.layers:
             vsk.stroke(stroke)
             vsk.geometry(GeometryCollection(layer))
             stroke = stroke+1
+        vsk.popMatrix()
 
-    def get(self, layer):
-        return GeometryCollection(self.layers[layer])
